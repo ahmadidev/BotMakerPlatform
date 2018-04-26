4a74bccdb2d084548593c758015f7b9c9e39992e
Erfan Ahmadi <ahmadierfan99@gmaill.com>
2018-04-26 12:55:32 +0430

    ConnectionManager Refactor

--- BotMakerPlatform.Web/Areas/SupportBot/SupportBotInstance.cs
+++ BotMakerPlatform.Web/Areas/SupportBot/SupportBotInstance.cs
@@ -10,13 +10,14 @@ namespace BotMakerPlatform.Web.Areas.SupportBot
     public class SupportBotInstance : IBotInstance
     {
         public int Id { get; set; }
+
         public ITelegramBotClient TelegramClient { get; set; }
 
         public IEnumerable<Subscriber> Subscribers { get; set; }
 
         public IEnumerable<Subscriber> Supporters { get; set; }
 
-        public IEnumerable<Connection> Connections { get; set; }
+        public ConnectionManager ConnectionManager { get; set; }
 
         public void Update(Update update, Subscriber subscriber)
         {
@@ -26,8 +27,7 @@ namespace BotMakerPlatform.Web.Areas.SupportBot
             var supporterRepo = new SupporterRepo(Id);
             Supporters = supporterRepo.GetAll();
 
-            var connectionRepo = new ConnectionRepo(Id);
-            Connections = connectionRepo.GetAll();
+            ConnectionManager = new ConnectionManager(Id, TelegramClient);
 
             Web.Controllers.HomeController.LogRecords.Add(subscriber.Username + " : " + update.Message.Text);
 
@@ -35,16 +35,6 @@ namespace BotMakerPlatform.Web.Areas.SupportBot
                 HandleSupporterMessage(update, subscriber);
             else
                 HandleUserMessage(update, subscriber);
-        }
-
-        private Subscriber SelectSupporter()
-        {
-            if (!Supporters.Any())
-                return null;
-
-            Subscriber minWaiterCountSupporter = Supporters.First();
-
-            return minWaiterCountSupporter;
         }
 
         public bool IsSupporter(Subscriber subscriber)
@@ -56,133 +46,24 @@ namespace BotMakerPlatform.Web.Areas.SupportBot
         {
             if (update.Message.Text == "/connect")
             {
-                QueueConnection(update, subscriber);
+                ConnectionManager.Connect(update, subscriber, Supporters.First());
             }
             else if (update.Message.Text == "/end")
             {
-                if (!EndConnection(subscriber))
+                if (!ConnectionManager.End(subscriber))
                 {
-                    //if (IsWaiting(subscriber))
-                    //    RemoveWaiter(subscriber);
-                    //else
                     TelegramClient.SendTextMessageAsync(subscriber.ChatId, "There is no connection to end");
                 }
             }
             else
             {
-                MessageOtherEnd(update, subscriber);
+                ConnectionManager.MessageOther(update, subscriber);
             }
         }
 
         private void HandleSupporterMessage(Update update, Subscriber supporter)
         {
-            MessageOtherEnd(update, supporter);
-        }
-
-        private void MessageOtherEnd(Update update, Subscriber subscriber)
-        {
-            if (FindUserConnection(subscriber.ChatId) != null)
-            {
-                long userEndId = FindUserConnectionEndChatId(subscriber);
-                TelegramClient.SendTextMessageAsync(userEndId, update.Message.Text);
-            }
-            else
-            {
-                TelegramClient.SendTextMessageAsync(subscriber.ChatId, "You have no current session.");
-            }
-        }
-
-        private void QueueConnection(Update update, Subscriber subscriber)
-        {
-            if (!Supporters.Any())
-                TelegramClient.SendTextMessageAsync(subscriber.ChatId,
-                    "We're sorry there is no available supporter at this time :(");
-            //else if (IsWaiting(subscriber))
-            //{
-            //    TelegramClient.SendTextMessageAsync(subscriber.ChatId,
-            //        "We're sorry, you're still on the waiting list :( You know what they say, Patience is the key to Everything :)");
-            //}
-            else
-            {
-                var supporter = SelectSupporter();
-
-                if (HasCurrentConnection(supporter.ChatId) && !HasCurrentConnection(subscriber.ChatId))
-                {
-                    //supporter.AddWaiter(TelegramClient, subscriber);
-                }
-                else
-                {
-                    if (HasCurrentConnection(subscriber.ChatId))
-                        TelegramClient.SendTextMessageAsync(subscriber.ChatId,
-                            "You are already connected. try talking to the supporter. Don't be shy kiddo");
-                    else
-                        AddConnection(subscriber, supporter);
-                }
-            }
-        }
-
-        private void AddConnection(Subscriber subscriber, Subscriber supporter)
-        {
-            var connectionRepo = new ConnectionRepo(Id);
-
-            connectionRepo.Add(new Connection
-            {
-                BotInstanceId = Id,
-                SupporterChatId = supporter.ChatId,
-                UserChatId = subscriber.ChatId
-            });
-
-            TelegramClient.SendTextMessageAsync(supporter.ChatId, "You are now connected to user : "
-                + subscriber.Username);
-
-            TelegramClient.SendTextMessageAsync(subscriber.ChatId, "You are now connected to supporter");
-        }
-
-        private bool EndConnection(Subscriber subscriber)
-        {
-            Connection connection = FindUserConnection(subscriber.ChatId);
-
-            if (connection == null)
-                return false;
-
-            var connectionRepo = new ConnectionRepo(Id);
-            connectionRepo.Remove(connection);
-
-            TelegramClient.SendTextMessageAsync(connection.SupporterChatId, "Your Connection has ended with "
-                + subscriber.Username);
-
-            TelegramClient.SendTextMessageAsync(connection.UserChatId, "Your Connection has ended");
-
-            return true;
-        }
-
-        private Connection FindUserConnection(long subscriberChatId)
-        {
-            return Connections.FirstOrDefault(connection => connection.UserChatId == subscriberChatId || connection.SupporterChatId == subscriberChatId);
-        }
-
-        private bool HasCurrentConnection(long subscriberChatId)
-        {
-            foreach (var connection in Connections)
-            {
-                if (connection.UserChatId == subscriberChatId || connection.SupporterChatId == subscriberChatId)
-                    return true;
-            }
-
-            return false;
-        }
-
-        private long FindUserConnectionEndChatId(Subscriber subscriber)
-        {
-            foreach (var connection in Connections)
-            {
-                if (connection.UserChatId == subscriber.ChatId)
-                    return connection.SupporterChatId;
-                if (connection.SupporterChatId == subscriber.ChatId)
-                    return connection.UserChatId;
-            }
-
-            return 0;
+            ConnectionManager.MessageOther(update, supporter);
         }
     }
 }
