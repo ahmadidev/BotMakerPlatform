d67ace5d93e1fe5a90b6ea7513b44681f357fc61
Ahmad Ahmadi <ahmadi.dt@gmail.com>
2018-11-25 18:24:17 +0330

    Try to save file on disk

--- BotMakerPlatform.Web/Areas/EjooUtilBot/EjooUtilBotInstance.cs
+++ BotMakerPlatform.Web/Areas/EjooUtilBot/EjooUtilBotInstance.cs
@@ -1,5 +1,8 @@
-﻿using BotMakerPlatform.Web.Areas.SupportBot.Manager;
+﻿using System.IO;
+using System.Web.Hosting;
+using BotMakerPlatform.Web.Areas.SupportBot.Manager;
 using BotMakerPlatform.Web.Areas.SupportBot.Repo;
+using BotMakerPlatform.Web.Controllers;
 using BotMakerPlatform.Web.Repo;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -43,9 +46,16 @@ namespace BotMakerPlatform.Web.Areas.EjooUtilBot
                     var file = TelegramClient.GetFileAsync(fileId).Result;
                     //TODO: get token
                     var url = "https://api.telegram.org/file/bot" + "Kossheer Token" + "/" + file.FilePath;
+
+                    HomeController.LogRecords.Add($"Path of fileId {file.FileId}: {file.FilePath}");
+
+                    using (var fileStream = new FileStream(HostingEnvironment.MapPath("~/image.jpg"), FileMode.Create))
+                    {
+                        var result = TelegramClient.GetInfoAndDownloadFileAsync(fileId, fileStream).Result;
+                    }
+
                     // PDF = Renderer.RenderUrlAsPdf("")
                 }
-
                 //TelegramClient.SendDocumentAsync(subscriber.ChatId, new InputOnlineFile(PDF.Stream));
             }
         }
